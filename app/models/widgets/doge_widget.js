de1b566734e982ffbce3c77c232c51bb9b72a2c9
vascogrilo <vascogrilo@gmail.com>
2014-04-09 14:15:46 +0100

    Simplify doge widget.

--- app/models/widgets/doge_widget.js
+++ app/models/widgets/doge_widget.js
@@ -1,35 +1,4 @@
-/**
- * Shows an arbitrary number along with an indicator showing if the number increased or decreased in
- * relation to its previous update.
- *
- * Expects data from sources as a single number.
- */
-Dashboard.DogeWidget = Dashboard.Widget.extend({
-  sourceData: 0,
-  previous: 0,
+Dashboard.DogeWidget = Dashboard.NumberWidget.extend({
   templateName: 'doge_widget',
-  classNames: ['widget', 'widget-doge'],
-  preUnit: "",
-  postUnit: "",
-
-  formattedValue: function() {
-    return this.get("preUnit") + this.get("content") + this.get("postUnit");
-  }.property("content"),
-
-  difference: function() {
-    var res = "";
-    if (this.get("previous") != 0) {
-      res = Math.abs(Math.round((this.get("content") - this.get("previous")) / this.get("previous") * 100));
-      res += "%";
-    }
-    this.set("previous", this.get("content"));
-    return res;
-  }.property("content"),
-
-  arrow: function() {
-    if (this.get("content") > this.get("previous"))
-      return "icon-arrow-up";
-    else
-      return "icon-arrow-down";
-  }.property("content")
+  classNames: ['widget', 'widget-doge']
 });
